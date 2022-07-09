c0aef214adc2393a82bbc0c2567cd7bbae9b6df4
pelumini <pelumini@gmail.com>
2022-07-09 10:08:38 +0100

    added tests and other configs

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+module.exports = createJestConfig(customJestConfig);
---
+++ src/components/Masthead/Masthead.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Masthead = () => {
+  return <div>Masthead</div>;
+};
